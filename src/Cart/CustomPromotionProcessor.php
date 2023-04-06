922658d71326a0fc1c3e6784979a822c751c15e5
d.neustadt <d.neustadt@shopware.com>
2023-04-06 10:38:55 +0200

    use absolute discount

--- src/Cart/CustomPromotionProcessor.php
+++ src/Cart/CustomPromotionProcessor.php
@@ -7,40 +7,63 @@ use Shopware\Core\Checkout\Cart\CartBehavior;
 use Shopware\Core\Checkout\Cart\CartProcessorInterface;
 use Shopware\Core\Checkout\Cart\LineItem\CartDataCollection;
 use Shopware\Core\Checkout\Cart\LineItem\LineItem;
-use Shopware\Core\Checkout\Cart\Price\PercentagePriceCalculator;
-use Shopware\Core\Checkout\Cart\Price\Struct\PercentagePriceDefinition;
+use Shopware\Core\Checkout\Cart\Price\AbsolutePriceCalculator;
+use Shopware\Core\Checkout\Cart\Price\Struct\CurrencyPriceDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Pricing\Price;
+use Shopware\Core\Framework\DataAbstractionLayer\Pricing\PriceCollection;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
 class CustomPromotionProcessor implements CartProcessorInterface
 {
-    private PercentagePriceCalculator $calculator;
+    private AbsolutePriceCalculator $calculator;
 
-    public function __construct(PercentagePriceCalculator $calculator)
+    public function __construct(AbsolutePriceCalculator $calculator)
     {
         $this->calculator = $calculator;
     }
 
     public function process(CartDataCollection $data, Cart $original, Cart $toCalculate, SalesChannelContext $context, CartBehavior $behavior): void
     {
-        if ($toCalculate->getLineItems()->filterGoods()->count() === 0) {
+        $discountLineItem = $this->createDiscount('EXAMPLE_DISCOUNT');
+
+        if ($toCalculate->getLineItems()->filterGoods()->count() < 2) {
+            if ($toCalculate->has($discountLineItem->getId())) {
+                $toCalculate->get($discountLineItem->getId())->setRemovable(true);
+                $toCalculate->remove($discountLineItem->getId());
+            }
+
             return;
         }
 
-        $discountLineItem = $this->createDiscount('EXAMPLE_DISCOUNT');
+        $goods = $toCalculate->getLineItems()->filterGoods();
+        $goods->sort(function (LineItem $a, LineItem $b) {
+            if ($a->getPrice()->getUnitPrice() === $b->getPrice()->getUnitPrice()) {
+                return 0;
+            }
+
+            if ($a->getPrice()->getUnitPrice() < $b->getPrice()->getUnitPrice()) {
+                return -1;
+            }
+
+            return 1;
+        });
+        $cheapest = $goods->first();
+        $cheapestPrice = -$cheapest->getPrice()->getUnitPrice();
 
         // declare price definition to define how this price is calculated
-        $definition = new PercentagePriceDefinition(-10);
+        $definition = new CurrencyPriceDefinition(new PriceCollection([
+            new Price($context->getCurrencyId(), $cheapestPrice, $cheapestPrice, false),
+        ]));
 
         $discountLineItem->setPriceDefinition($definition);
-
         // calculate price
         $discountLineItem->setPrice(
-            $this->calculator->calculate($definition->getPercentage(), $toCalculate->getLineItems()->getPrices(), $context)
+            $this->calculator->calculate($cheapestPrice, $goods->getPrices(), $context)
         );
 
         if ($original->has($discountLineItem->getId())) {
-            $original->get($discountLineItem->getId())->setRemovable(true);
-            $original->remove($discountLineItem->getId());
+            $original->get($discountLineItem->getId())->setPriceDefinition($discountLineItem->getPriceDefinition());
+            $original->get($discountLineItem->getId())->setPrice($discountLineItem->getPrice());
 
             return;
         }
